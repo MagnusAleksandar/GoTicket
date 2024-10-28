131bcffd2c1533e5ed1d820cb6e2ae2359eef7d5
MagnusAleksandar <alevel.col@gmail.com>
2024-10-28 16:12:07 -0500

    Agregado ícono de título

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require ("logica/Proveedor.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GoTicket</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/index.css">
